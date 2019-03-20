786715cab6155a03e1daa80ba7bd361110dd073f
miclis <mlgt007@gmail.com>
2019-03-20 16:33:01 +0100

    Highlighting fixed

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,7 +12,12 @@ export const clearResults = () => {
 };
 
 export const highlightSelected = id => {
-	document.querySelector(`a[href=#${id}]`).classList.add('.results__link--active');
+	const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+	resultsArr.forEach(el => {
+		el.classList.remove('results__link--active');
+	});
+
+	document.querySelector(`a[href*="${id}"]`).classList.add('results__link--active');
 };
 
 // Cuts the title into words and then joins again until the length is <= limit
